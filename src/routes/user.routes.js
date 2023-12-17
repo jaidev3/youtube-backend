eca5ccb12f78ed336c572b4a232bbf7d0fad8954
Jaidev <jaidevyadav720@gmail.com>
2023-12-17 23:55:16 +0530

    user login and logout methjod created along with auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,5 +17,10 @@ router.route("/register").post(
   registerUser
 );
 //  router.route("./login").post(loginUser);
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
