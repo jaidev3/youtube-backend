b6a6586e379c30cfb697af17ce24c4035762f8cf
jaidev <jaidevyadav720@gmail.com>
2024-01-13 00:18:46 +0530

    code refactoring and sudo code for controlers added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import dotenv from "dotenv";
-import { app } from "./app.js";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-  path: "./env",
-});
+    path: './.env'
+})
+
+
+
 connectDB()
-  .then(() => {
+.then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("mongodb fail", err);
-  });
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
@@ -24,28 +27,25 @@ connectDB()
 
 
 
-  
-// import mongoose from "mongoose";
-// const dotenv = require("dotenv");
-// // import { DB_NAME } from "./constants";
-// import express from "express";
-// // require("dotenv").config();
-// dotenv.config();
-// const app = express();
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}`);
-//     app.on("error", (error) => {
-//       console.log("Error ", error);
-//       throw error;
-//     });
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
 
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listning on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("Error ", error);
-//     throw error;
-//   }
-// })();
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
